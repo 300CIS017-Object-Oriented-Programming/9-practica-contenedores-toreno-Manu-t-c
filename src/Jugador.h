edcdddc5b91515b48a1cc679a104c73851e58190
Manuela <trujillomanuela9@gmail.com>
2025-04-02 10:05:09 -0500

    NEW: Mayor implementacion de métodos

---
+++ src/Jugador.h
@@ -0,0 +1,28 @@
+#ifndef JUGADOR_H
+#define JUGADOR_H
+#include <vector>
+#include <string>
+#include "Videojuego.h"
+
+class Jugador {
+    private:
+        std::string nickname;
+        int nivelRanking;
+        std::vector< Videojuego* > videojuegosInscrito;
+
+    public:
+      Jugador();
+     ~Jugador();
+      Jugador(std::string nickname, int nivelRanking);
+      std::string getNickname();
+    void setnickname(std::string &nickname);
+    void VideojuegosIncritos();
+
+    void mostar();
+
+    void inscribir(Videojuego* juego);
+};
+
+
+
+#endif //JUGADOR_H
