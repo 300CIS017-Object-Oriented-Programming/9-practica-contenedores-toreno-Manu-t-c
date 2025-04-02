48ae7e8bc01397ad2c01359efb4c0dadafde3b2f
Manuela <trujillomanuela9@gmail.com>
2025-04-02 10:27:07 -0500

    NEW: Main.cpp inicializada

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,61 @@
 #include <iostream>
+#include "Torneo.h"
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
-
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
-    }
-
-    return 0;
+using namespace std;
+void mostrarTorneoMenu() {
+    int opc = 0;
+    cout << "1. Registrar un videojuego\n";
+    cout << "2. Registrar un jugador\n";
+    cout << "3. inscribirse en un videojuego \n";
+    cout << "4. Agregar un videojuego \n";
+    cout << "5. Agregar un jugador \n";
+    cout << "-1. Volver\n";
+    cin >> opc;
 }
 
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
+void mostrarVideojuegosMenu() {
+    int opc = 0;
+    cout << "1. Mostrar información del videojuego\n";
+    cout << "-1. Volver\n";
+    cin >> opc;
+}
+
+void mostrarJugadorMenu() {
+    int opc = 0;
+    cout << "1. Mostrar Nickname y ranking\n";
+    cout << "2. Ver videojuegos inscritos\n";
+    cout << "3. Inscribirse \n";
+    cout << "-1. Volver\n";
+    cin >> opc;
+
+    cin>>(opc);
+}
+
+void mostrarMenu(Torneo &torneoMenu) {
+
+    int opc = 0;
+    do
+    {
+        cout << "\n ****Bienvenidos ****\n";
+        cout << "1. Mostrar menu Torneo \n";
+        cout << "2. Mostrar menu Jugador \n";
+        cout << "3. Mostrar menu Videojuegos \n";
+        cout << "-1. Salir \n";
+
+        cin >> opc;
+
+        switch (opc)
+        {
+            case 1:
+                mostrarTorneoMenu();
+            break;
+            case 2:
+                mostrarJugadorMenu();
+            break;
+            case 3:
+                mostrarJugadorMenu();
+            default:
+                break;
+        }
+    } while (opc != -1);
+}
